0eae126ae999e153a6db90c38e0107292d40fe6b
Terrini <Haileykayy69@gmail.com>
2024-04-24 14:12:38 -0400

    First checkpoint/Bella.Obstacle Course

--- ObstacleCourse.js
+++ ObstacleCourse.js
@@ -1,6 +1,16 @@
-// programmer: hailey 
-// Team Member: Bellioso
-// Bolt ID:B2F8
+// Programmer: Bella
+// Team Member: Hailioso
+// Bolt ID: B2F8
 // Date: 4.24.2024
 // Program: Obstacle Course
-// Program URL:https://edu.sphero.com/program/16752570/edit
+// Program URL: https://edu.sphero.com/program/16752511/edit
+
+
+//Bella Williams
+async function startProgram() {
+	setMainLed({ r: 255, g: 0, b: 0});
+	await speak("Booting obstacle course scan, scan completed, starting course.", true)
+	await roll((getHeading() -180), 80, 2.5);
+	await delay(2);
+	
+}
