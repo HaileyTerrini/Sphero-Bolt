fa679ba36e65d1ea3069a5e6452efed8d1e0e61b
Terrini <Haileykayy69@gmail.com>
2024-04-26 13:47:23 -0400

    // Hailey- PC #6 - Checkpoint- Checkpoint TWO - blue

--- ObstacleCourse.js
+++ ObstacleCourse.js
@@ -1,4 +1,4 @@
-// Programmer: Hailey
+// Programmer: Haile
 // Team Member: Bella
 // Bolt ID: B2F8
 // Date: 4.24.2024
@@ -6,11 +6,20 @@
 // Program URL: https://edu.sphero.com/program/16752511/edit
 
 
-//Bella Williams
+//Bella Williams- PC #5 - Checkpoint ONE - red
 async function startProgram() {
 	setMainLed({ r: 255, g: 0, b: 0});
+	await Sound.Animal.Monkey.play(true);
 	await speak("Booting obstacle course scan, scan completed, starting course.", true)
 	await roll((getHeading() -180), 80, 2.5);
-	await delay(2);
+	await delay(3);
 	
+// Hailey- PC #6 - Checkpoint- Checkpoint TWO - blue
+	
+	setMainLed({ r: 0, g: 0, b: 255});
+	await Sound.Animal.HorseGallop.play(true);
+	await speak("Checkpoint Two Commencing", true)
+	await roll((getHeading() -270), 70, 2.5);
+	
+	await delay(3);
 }
